bc480b093f10515ffaefa56d6c1a05224b4f7d94
Krishnamoorthy Venkatraman <krishna_v14@hotmail.com>
2016-04-20 16:52:49 -0500

    Made the header

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,59 @@ var runSequence = require('run-sequence');
 var cleanCss = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
+
+/*
+  * Clean task to delete the dist dir
+*/
+gulp.task('clean',function(){
+ return del([config.dist]);
+});
+
+/*
+* Copy minified bootstrap to dist folder from node modules
+*/
 gulp.task('copyMinBootstrapToDist',function(){
   return gulp.src(config.nodeSrc+config.bootstrapPath+'dist/css/bootstrap.min.css')
   .pipe(gulp.dest(config.dist+'external/css'));
 });
-gulp.task('minifyNormalizeToDist',function(){
+
+/*
+* Copy minimified normalize to the dist folder from node modules
+*/
+gulp.task('copyMinNormalizeToDist',function(){
   return gulp.src(config.nodeSrc+config.normalizePath+"normalize.css")
   .pipe(rename('normalize.min.css'))
   .pipe(gulp.dest(config.dist+'external/css'));
 });
 
-gulp.task('clean',function(){
- return del([config.dist]);
-});
-
+/*
+* Create images in different sizes
+*/
 gulp.task('create-responsive',function(){
   return gulp.src(config.src+"img_src/responsive/"+"**/*.{png,jpeg,jpg}")
   .pipe(gulp.dest(config.dist+"img/"));
 });
 
+/*
+*
+*/
 gulp.task('copyHtmlToDist',function(){
   return gulp.src(config.src+"**/*.html")
     .pipe(gulp.dest(config.dist));
 });
 
+/*
+*
+*/
 gulp.task('copyImgToDist',['create-responsive'],function() {
   return gulp.src(config.src+"img_src/**[^responsive]/*")
     .pipe(gulp.dest(config.dist+"img/"));
 });
 
-gulp.task('minifyCss',['copyMinBootstrapToDist','minifyNormalizeToDist'],function(){
+/*
+* Minify Css and copy to dist
+*/
+gulp.task('minifyCss',['copyMinBootstrapToDist','copyMinNormalizeToDist'],function(){
   return gulp.src(config.src+"css/**/*")
     .pipe(sourcemaps.init())
     .pipe(cleanCss())
